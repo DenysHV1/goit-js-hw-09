f78613974a56f0028204465f39f81b25b4689231
DenVik <denyagarkusha2@gmail.com>
2024-07-28 14:16:20 +0200

    ready

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,72 +4,58 @@ const message = document.querySelector('.message');
 const done = document.querySelector('.text');
 const emailError = document.querySelector('.error');
 const emptyError = document.querySelector('.empty-error');
-class ClassData {
-  constructor(email, message) {
-    this.email = email;
-    this.message = message;
-  }
+
+const formData = {
+  userEmail: '',
+  userMessage: '',
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  const storedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  inputEmail.value = storedData.email;
+  message.value = storedData.message;
 }
 
 form.addEventListener('submit', oneClickSend);
 
 function oneClickSend(event) {
   event.preventDefault();
-  const email = event.target.elements.email.value;
-  const message = event.target.elements.message.value;
-  const array = [
-    '!',
-    '£',
-    '$',
-    '%',
-    '^',
-    '&',
-    '*',
-    '(',
-    ')',
-    '_',
-    '-',
-    '+',
-    '=',
-    '#',
-    '~',
-    '/',
-    '?',
-    '{',
-    '}',
-    '[',
-    ']',
-    '`',
-    '¬',
-    '|',
-    '"',
-    ',',
-    '<',
-    '>',
-    ':',
-    ';',
-  ];
+
+  const email = event.target.elements.email.value.trim().toLowerCase();
+  const message = event.target.elements.message.value.trim();
+
+    //перевірка 1
+	const array = ['!','£','$','%','^','&','*','(',')','_','-','+','=','#','~','/','?','{','}','[',']','`','¬','|','"',',','<','>',':',';',];
+
   for (let i = 0; i < array.length; i += 1) {
     if (email.includes(array[i])) {
       emailError.textContent = `Invalid character in name: ${array[i]}`;
       return emailError.textContent;
     }
   }
+
   emailError.textContent = '';
 
-  if (email.length === 0) {
-    return (emptyError.textContent = 'Fill please all fields');
+  //перевірка 2
+
+  if (!email || !message) {
+    done.textContent = '';
+    emptyError.textContent = 'Please fill in all fields';
+    return;
   }
+
   emptyError.textContent = '';
 
-  if (message.length === 0) {
-    return (emptyError.textContent = 'Fill please all fields');
-  }
-  emptyError.textContent = '';
+  //result
+  const formData = {
+    userEmail: email,
+    userMessage: message,
+  };
 
-  const formData = new ClassData(email.trim().toLowerCase(), message.trim());
-  console.log(formData);
   done.textContent = 'Success!';
+  console.log(formData);
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
 }
 
 const feedbackFormState = {
